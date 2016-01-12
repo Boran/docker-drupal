05d119df85d301c93f9ad342b92f1f2cef1a5c8c
Sean Boran <sean@boran.com>
2016-01-12 16:50:34 +0100

    simplify

--- files/postfix.sh
+++ files/postfix.sh
@@ -24,9 +24,9 @@ trap "postfix stop"   SIGTERM
 trap "postfix reload" SIGHUP
 
 # force new copy of hosts there (otherwise links could be outdated)
-cp -f /etc/hosts /var/spool/postfix/etc/hosts
-cp -f /etc/services /var/spool/postfix/etc/services
-cp -f /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
+cp -f /etc/hosts /var/spool/postfix/etc
+cp -f /etc/services /var/spool/postfix/etc
+cp -f /etc/resolv.conf /var/spool/postfix/etc
 
 # start postfix
 postfix start
